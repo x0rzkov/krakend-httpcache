afa5ba2321e56e75449c7e1f317705039bb213cd
x0rzkov <x0rzkov@protonmail.com>
2020-07-15 09:42:07 +0000

    Add diskcache backend storage

--- http.go
+++ http.go
@@ -9,24 +9,42 @@ import (
 	"github.com/devopsfaith/krakend/proxy"
 	"github.com/devopsfaith/krakend/transport/http/client"
 	"github.com/gregjones/httpcache"
+	"github.com/gregjones/httpcache/diskcache"
 )
 
 // Namespace is the key to use to store and access the custom config data
-const Namespace = "github.com/devopsfaith/krakend-httpcache"
+const Namespace = "github.com/x0rzkov/krakend-httpcache"
 
 var (
-	memTransport = httpcache.NewMemoryCacheTransport()
-	memClient    = http.Client{Transport: memTransport}
+	cacheTransport httpcache.Cache
+	cacheClient    = http.Client{}
 )
 
 // NewHTTPClient creates a HTTPClientFactory using an in-memory-cached http client
-func NewHTTPClient(cfg *config.Backend) client.HTTPClientFactory {
+func NewHTTPClient(cfg *config.Backend, store string) client.HTTPClientFactory {
 	_, ok := cfg.ExtraConfig[Namespace]
 	if !ok {
 		return client.NewHTTPClient
 	}
+
+	switch store {
+	case "disk":
+		diskCachePath := "./shared/data/cache/krakend/httpcache"
+		err = os.MkdirAll(diskCachePath, os.ModePerm)
+		if err != nil {
+			log.Fatal("ERROR:", err.Error())
+		}
+		backend := diskcache.New(diskCachePath)
+		cacheTransport = httpcache.NewTransport(backend)
+  	case "memory":
+	  fallthrough
+        default:
+		cacheTransport = httpcache.NewMemoryCacheTransport()
+	}
+
+	cacheClient.Transport = cacheTransport
 	return func(_ context.Context) *http.Client {
-		return &memClient
+		return &cacheClient
 	}
 }
 
